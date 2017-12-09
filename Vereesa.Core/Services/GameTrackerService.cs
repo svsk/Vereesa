5f34de7e449938dc9eb5519595d02931b1cab044
Sverre Skuland <sverre.skuland@gmail.com>
2017-12-09 11:57:15 +0100

    Added code to track game activity.

--- Vereesa.Core/Services/GameTrackerService.cs
+++ Vereesa.Core/Services/GameTrackerService.cs
@@ -12,16 +12,40 @@ namespace Vereesa.Core.Services
         public GameTrackerService(DiscordSocketClient discord)
         {
             _discord = discord;
+            _discord.GuildAvailable += OnGuildAvailable;
             _discord.GuildMemberUpdated += OnMemberUpdated;
         }
 
-        private async Task OnMemberUpdated(SocketGuildUser before, SocketGuildUser after)
+        private async Task OnGuildAvailable(SocketGuild arg)
         {
-            if (after.Game != null) {
-                //User has started playing game
-            } else if (before.Game != null && after.Game == null) {
-                //User is no longer playing game
+            //Do initial scan
+
+        }
+
+        private async Task OnMemberUpdated(SocketGuildUser userBeforeChange, SocketGuildUser userAfterChange)
+        {
+            var beforeGame = userBeforeChange.Game;
+            var afterGame = userAfterChange.Game;
+
+            if (beforeGame != null && (afterGame == null || afterGame.Value.Name != beforeGame.Value.Name))
+            {
+                StoppedPlaying(userBeforeChange);
             }
+            
+            if (afterGame != null)
+            {
+                StartedPlaying(userAfterChange);
+            }
+        }
+
+        private void StartedPlaying(SocketGuildUser user)
+        {
+            System.IO.File.AppendAllText(@"C:\temp\gamelog.json", $"{DateTime.UtcNow} | {user.Username} started playing {user.Game.Value.Name}.{Environment.NewLine}");
+        }
+
+        private void StoppedPlaying(SocketGuildUser user)
+        {
+            System.IO.File.AppendAllText(@"C:\temp\gamelog.json", $"{DateTime.UtcNow} | {user.Username} stopped playing {user.Game.Value.Name}.{Environment.NewLine}");
         }
     }
 }
