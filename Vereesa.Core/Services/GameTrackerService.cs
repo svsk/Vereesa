aba09d4644d1546b395ccd538e1dbc885325bfa5
Sverre Skuland <sverre.skuland@gmail.com>
2017-12-12 10:25:06 +0100

    Emit game state from game tracker to external endpoint

--- Vereesa.Core/Services/GameTrackerService.cs
+++ Vereesa.Core/Services/GameTrackerService.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using Vereesa.Data;
 using Vereesa.Data.Models.GameTracking;
 
@@ -13,34 +16,43 @@ namespace Vereesa.Core.Services
     {
         private DiscordSocketClient _discord;
         private JsonRepository<GameTrackMember> _trackingRepo;
+        private IConfigurationRoot _options;
 
-        public GameTrackerService(DiscordSocketClient discord, JsonRepository<GameTrackMember> trackingRepo)
+        public GameTrackerService(DiscordSocketClient discord, JsonRepository<GameTrackMember> trackingRepo, IConfigurationRoot options)
         {
             _discord = discord;
             _trackingRepo = trackingRepo;
             _discord.GuildAvailable += OnGuildAvailable;
             _discord.GuildMemberUpdated += OnMemberUpdated;
+            _options = options;
         }
 
-        private async Task OnGuildAvailable(SocketGuild arg)
+        private async Task OnGuildAvailable(SocketGuild guild)
         {
-            //Do initial scan
-
+            await EmitGameState(guild);
         }
 
         private async Task OnMemberUpdated(SocketGuildUser userBeforeChange, SocketGuildUser userAfterChange)
         {
+            var gameChangeHappened = false;
             var beforeGame = userBeforeChange.Game;
             var afterGame = userAfterChange.Game;
 
             if (beforeGame != null && (afterGame == null || afterGame.Value.Name != beforeGame.Value.Name))
             {
                 UpdateUserGameState(userBeforeChange, "stopped");
+                gameChangeHappened = true;
             }
             
             if (afterGame != null)
             {
                 UpdateUserGameState(userAfterChange, "started");
+                gameChangeHappened = true;
+            }
+
+            if (gameChangeHappened) 
+            {
+                await EmitGameState(userAfterChange.Guild);
             }
         }
 
@@ -66,6 +78,35 @@ namespace Vereesa.Core.Services
             }
 
             return member;
+        }
+
+        private  async Task EmitGameState(SocketGuild guild) 
+        {            
+            var gameNames = guild.Users.Where(u => u.Game != null && u.IsBot == false)
+                .Select(u => u.Game.Value.Name)
+                .GroupBy(n => n)
+                .OrderByDescending(g => g.Count());
+
+            var gameState = new List<object>();
+
+            foreach (var game in gameNames) 
+            {
+                gameState.Add(new {
+                    name = game.Key,
+                    count = game.Count()
+                });
+            }
+
+            var postContent = new Dictionary<string, string>();
+            postContent.Add("gamestate", JsonConvert.SerializeObject(gameState));
+
+            using (var client = new HttpClient()) 
+            {
+                client.DefaultRequestHeaders.Add("user-key", _options["gameStateEmissions:emissionEndpointUserKey"]);
+                var response = await client.PostAsync(_options["gameStateEmissions:emissionEndpoint"], new FormUrlEncodedContent(postContent));
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseContent);
+            }
         }
     }
 }
