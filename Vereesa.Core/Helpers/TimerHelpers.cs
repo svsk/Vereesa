3e8f67f236497aa38d90a2daf1f8b87f0225664b
Sverre Skuland <sverre.skuland@gmail.com>
2018-01-23 18:38:34 +0100

    Added auto refresh option to SetTimeout

--- Vereesa.Core/Helpers/TimerHelpers.cs
+++ Vereesa.Core/Helpers/TimerHelpers.cs
@@ -5,11 +5,11 @@ namespace Vereesa.Core.Helpers
 {
     public class TimerHelpers
     {
-        public static Timer SetTimeout(Action predicate, int interval) 
+        public static Timer SetTimeout(Action predicate, int interval, bool autoRefresh = false) 
         {
             var timer = new Timer(interval);
             timer.Elapsed += (object sender, ElapsedEventArgs args) => { predicate.Invoke(); };
-            timer.AutoReset = false;
+            timer.AutoReset = autoRefresh;
             timer.Start();
 
             return timer;
