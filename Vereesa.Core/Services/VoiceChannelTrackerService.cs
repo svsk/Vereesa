1bb7011534865d4193d29e9368fe2ebbdf57c21a
Sverre Skuland <sverre.skuland@gmail.com>
2018-01-23 18:42:21 +0100

    Delete voice chat log messages older than 3h

--- Vereesa.Core/Services/VoiceChannelTrackerService.cs
+++ Vereesa.Core/Services/VoiceChannelTrackerService.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
+using Discord;
 using Discord.WebSocket;
 using Vereesa.Core.Configuration;
 using Vereesa.Core.Extensions;
+using Vereesa.Core.Helpers;
 
 namespace Vereesa.Core.Services
 {
@@ -11,12 +15,28 @@ namespace Vereesa.Core.Services
         private DiscordSocketClient _discord;
         private VoiceChannelTrackerSettings _settings;
         private ISocketMessageChannel _announcementChannel;
+        private Timer _clearOldMessagesInterval;
 
-        public VoiceChannelTrackerService(DiscordSocketClient discord, VoiceChannelTrackerSettings settings) 
+        public VoiceChannelTrackerService(DiscordSocketClient discord, VoiceChannelTrackerSettings settings)
         {
             _discord = discord;
             _discord.GuildAvailable += InitailizeVoiceChannelTracker;
             _settings = settings;
+            _clearOldMessagesInterval = TimerHelpers.SetTimeout(async () => { await DeleteOldMessages(); }, 120000, true);
+        }
+
+        private async Task DeleteOldMessages()
+        {
+            if (_announcementChannel == null)
+                return;
+
+            var messages = await _announcementChannel.GetMessagesAsync(1000).Flatten();
+            foreach (var message in messages.Where(c => c.Author.IsBot))
+            {
+                if (message.Timestamp < DateTimeOffset.UtcNow.AddHours(-3)) {
+                    await message.DeleteAsync();
+                }
+            }
         }
 
         private async Task InitailizeVoiceChannelTracker(SocketGuild guild)
@@ -24,6 +44,7 @@ namespace Vereesa.Core.Services
             _discord.UserVoiceStateUpdated -= HandleVoiceStateChanged;
             _discord.UserVoiceStateUpdated += HandleVoiceStateChanged;
             _announcementChannel = guild.GetChannelByName(_settings.AnnouncementChannelName);
+            await DeleteOldMessages();
         }
 
         private async Task HandleVoiceStateChanged(SocketUser user, SocketVoiceState beforeChangeState, SocketVoiceState afterChangeState)
@@ -32,17 +53,23 @@ namespace Vereesa.Core.Services
             var afterChannel = afterChangeState.VoiceChannel;
 
             var action = string.Empty;
-            if (beforeChannel != null && afterChannel == null) {
+            if (beforeChannel != null && afterChannel == null)
+            {
                 action = $"left `{beforeChannel.Name}`";
             }
-                
-            if (beforeChannel != null && afterChannel != null) {
+
+            if (beforeChannel != null && afterChannel != null)
+            {
                 action = $"switched from `{beforeChannel.Name}` to `{afterChannel.Name}`";
             }
-                
-            if (beforeChannel == null && afterChannel != null) {
+
+            if (beforeChannel == null && afterChannel != null)
+            {
                 action = $"joined `{afterChannel.Name}`";
             }
+
+            if (beforeChannel?.Id == afterChannel?.Id)
+                return;
 
             await _announcementChannel.SendMessageAsync($"{user.Username} {action}.", isTTS: true);
         }
