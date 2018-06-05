f11b9da62e0becf27e6aa6b95b377c9151badc40
Sverre Skuland <sverre.skuland@gmail.com>
2018-06-05 13:29:58 +0200

    Added a test project for Core

--- Vereesa.Data/Models/EventHub/EventHubEvents.cs
+++ Vereesa.Data/Models/EventHub/EventHubEvents.cs
@@ -3,5 +3,6 @@ namespace Vereesa.Data.Models.EventHub
     public class EventHubEvents 
     {
         public const string NewCsvRow = "NewCsvRow";
+        public const string Test = "Test";
     }
 }
