b9d9b44ff669b27792b96ffe74ca8c3d3e539e2a
Sverre Skuland <sverre.skuland@gmail.com>
2018-07-03 13:28:38 +0200

    Add logging in JsonRepository. Try to make it work in Azure WebJob.

--- Vereesa.Data/Repositories/JsonRepository.cs
+++ Vereesa.Data/Repositories/JsonRepository.cs
@@ -16,18 +16,30 @@ namespace Vereesa.Data.Repositories
         public JsonRepository()
         {
             _filePath = $@"{AppContext.BaseDirectory}/data";
-            if (Directory.Exists(_filePath) == false)
+            if (Directory.Exists(_filePath) == false) 
             {
                 Directory.CreateDirectory(_filePath);
             }
 
+            // #if !DEBUG 
+            //     ConfigureForAzure();
+            // #endif
+
             _context = new List<T>();
+
+            Console.WriteLine($"Initialized JsonRepository for type {typeof(T).Name} at {_filePath}.");
+        }
+
+        private List<FileInfo> GetRepositoryFiles() 
+        {
+            var directory = new DirectoryInfo(_filePath);
+            var files = directory.EnumerateFiles($"{typeof(T).Name}.*.json").ToList();
+            return files;
         }
 
         public IEnumerable<T> GetAll()
         {
-            var directory = new DirectoryInfo(_filePath);
-            var files = directory.EnumerateFiles($"{typeof(T).Name}.*.json").ToList();
+            var files = GetRepositoryFiles();
 
             foreach (var file in files)
             {
@@ -44,14 +56,34 @@ namespace Vereesa.Data.Repositories
 
         public void Add(T entity)
         {
+            Console.WriteLine($"Adding new entity of type {typeof(T).Name}.");
             _context.Add(entity);
         }
 
         public void Save()
         {
+            Console.WriteLine($"Saving entities of type {typeof(T).Name}.");
+
+            var savedEntities = 0;
+
             foreach (var entity in _context)
             {
-                File.WriteAllText($"{_filePath}/{typeof(T).Name}.{entity.Id}.json", JsonConvert.SerializeObject(entity));
+                try 
+                {
+                    File.WriteAllText($"{_filePath}/{typeof(T).Name}.{entity.Id}.json", JsonConvert.SerializeObject(entity));
+                    savedEntities++;
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine($"Failed save entity: {entity.Id} ({typeof(T).Name}).", ex);
+                }
+            }
+
+            Console.WriteLine($"Saved {savedEntities} entities of type {typeof(T).Name}.");
+
+            if (_isAzure) 
+            {
+                SaveToWebJobDirectoryIfStale();
             }
         }
 
@@ -64,6 +96,34 @@ namespace Vereesa.Data.Repositories
             else
             {
                 Add(entity);
+            }
+        }
+
+        private string _azureData = "D:\\home\\site\\wwwroot\\App_Data\\jobs\\continuous\\Vereesa\\data";
+        private DateTime _lastSaveTime = DateTime.Now;
+        private bool _isAzure = false;
+
+        private void ConfigureForAzure() 
+        {
+            Console.WriteLine("Welcome to Azure!");
+            _isAzure = true;
+        }
+
+        private void SaveToWebJobDirectoryIfStale() 
+        {
+            var timeSinceLastSave = DateTime.Now - _lastSaveTime;
+            if (timeSinceLastSave.TotalMinutes > 30) 
+            {
+                Console.WriteLine("Detected a stale backing cache. Copying files to Web Job directory.");
+                _lastSaveTime = DateTime.Now;
+
+                var files = GetRepositoryFiles();
+                foreach (var file in files) 
+                {
+                    file.CopyTo($"{_azureData}/{file.Name}");
+                }
+
+                Console.WriteLine("Finished copying files to Web Job directory.");
             }
         }
     }
