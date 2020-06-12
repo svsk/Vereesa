111425bed5176ce68929ddd0c0e62eb53f7b2a77
Sverre Skuland <sverre.skuland@gmail.com>
2020-06-12 15:21:41 +0200

    Add BlueTrackerService.

---
+++ Vereesa.Core/Services/BlueTrackerService.cs
@@ -0,0 +1,12 @@
+using Discord.WebSocket;
+
+namespace Vereesa.Core.Services
+{
+	public class BlueTrackerService : BotServiceBase
+	{
+		public BlueTrackerService(DiscordSocketClient discord)
+			: base(discord)
+		{
+		}
+	}
+}
