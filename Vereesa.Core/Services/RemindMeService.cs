9512e183b104ebdaa9ade48f8e73b00aad1de167
Sverre Skuland <sverre.skuland@gmail.com>
2021-05-07 22:11:18 +0200

    Add periodic reminders.

--- Vereesa.Core/Services/RemindMeService.cs
+++ Vereesa.Core/Services/RemindMeService.cs
@@ -2,42 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Timers;
 using Discord;
-using Discord.WebSocket;
-using Vereesa.Core.Helpers;
 using Vereesa.Data.Interfaces;
 using Vereesa.Data.Models.Reminders;
 using Vereesa.Core.Infrastructure;
+using Vereesa.Core.Extensions;
+using NodaTime;
+using Discord.WebSocket;
 
 namespace Vereesa.Core.Services
 {
 	public class RemindMeService : BotServiceBase
 	{
-		private DiscordSocketClient _discord;
-		private Timer _timer;
 		private IRepository<Reminder> _reminderRepository;
-		private string _commandWord = "!remindme";
+		private const string _commandWord = "!remindme";
 
-		public RemindMeService(DiscordSocketClient discord, IRepository<Reminder> reminderRepository)
+		public RemindMeService(DiscordSocketClient discord, IJobScheduler scheduler,
+			IRepository<Reminder> reminderRepository)
 			: base(discord)
 		{
-			_discord = discord;
-			_discord.Ready -= InitializeAsync;
-			_discord.Ready += InitializeAsync;
 			_reminderRepository = reminderRepository;
+			scheduler.EveryTenSeconds += AnnounceElapsedIntervalsAsync;
 		}
 
-		private async Task InitializeAsync()
-		{
-			_timer?.Stop();
-			_timer?.Dispose();
-			_timer = await TimerHelpers.SetTimeoutAsync(HandleCheckIntervalElapsedAsync, 10000, true, true);
-			_discord.MessageReceived -= HandleMessageReceived;
-			_discord.MessageReceived += HandleMessageReceived;
-		}
-
-		private async Task HandleMessageReceived(SocketMessage receivedMessage)
+		[OnCommand(_commandWord)]
+		public async Task HandleMessageReceived(IMessage receivedMessage)
 		{
 			if (TryParseReminder(receivedMessage, out var reminder))
 			{
@@ -48,7 +37,7 @@ namespace Vereesa.Core.Services
 
 		private IMessageChannel GetChannel(ulong channelId)
 		{
-			var channel = _discord.GetChannel(channelId);
+			var channel = Discord.GetChannel(channelId);
 			return (IMessageChannel)channel;
 		}
 
@@ -63,7 +52,7 @@ namespace Vereesa.Core.Services
 			await _reminderRepository.AddAsync(reminder);
 		}
 
-		private bool TryParseReminder(SocketMessage message, out Reminder reminder)
+		private bool TryParseReminder(IMessage message, out Reminder reminder)
 		{
 			reminder = null;
 			var msgContent = message.Content;
@@ -99,45 +88,49 @@ namespace Vereesa.Core.Services
 
 			try
 			{
-				var split = reminderTime.Split(" ");
-				var number = int.Parse(split[0]);
-				var unit = split[1].ToLowerInvariant();
 				var now = DateTimeOffset.UtcNow;
 				var remindTime = now;
 
-				if (unit.Contains("second"))
+				if (!TryParseDuration(reminderTime, out var duration))
 				{
-					remindTime = now.AddSeconds(number);
-				}
-				else if (unit.Contains("minute"))
-				{
-					remindTime = now.AddMinutes(number);
-				}
-				else if (unit.Contains("hour"))
-				{
-					remindTime = now.AddHours(number);
-				}
-				else if (unit.Contains("day"))
-				{
-					remindTime = now.AddDays(number);
-				}
-				else if (unit.Contains("month"))
-				{
-					remindTime = now.AddMonths((int)number);
-				}
-				else if (unit.Contains("year"))
-				{
-					remindTime = now.AddYears((int)number);
-				}
-				else
-				{
-					throw new InvalidOperationException("Couldn't figure out time unit.");
+					throw new InvalidCastException("Couldn't cast string to duration.");
 				}
 
-				remindUnixTimestamp = remindTime.ToUnixTimeSeconds();
+				remindUnixTimestamp = SystemClock.Instance.GetCurrentInstant().Plus(duration).ToUnixTimeSeconds();
 			}
 			catch
 			{
+				return false;
+			}
+
+			return true;
+		}
+
+		private bool TryParseDuration(string reminderTime, out Duration result)
+		{
+			var split = reminderTime.Split(" ");
+			var number = int.Parse(split[0]);
+			var unit = split[1].ToLowerInvariant();
+
+			if (unit.Contains("second"))
+			{
+				result = Duration.FromSeconds(number);
+			}
+			else if (unit.Contains("minute"))
+			{
+				result = Duration.FromMinutes(number);
+			}
+			else if (unit.Contains("hour"))
+			{
+				result = Duration.FromHours(number);
+			}
+			else if (unit.Contains("day"))
+			{
+				result = Duration.FromDays(number);
+			}
+			else
+			{
+				result = Duration.MaxValue;
 				return false;
 			}
 
@@ -154,7 +147,7 @@ namespace Vereesa.Core.Services
 			return msgContent.Replace($"{_commandWord} ", string.Empty).Replace($"\"{reminderMessage}\"", string.Empty).ToLowerInvariant();
 		}
 
-		private async Task HandleCheckIntervalElapsedAsync()
+		private async Task AnnounceElapsedIntervalsAsync()
 		{
 			var overdueReminders = await GetOverdueRemindersAsync();
 
@@ -162,7 +155,18 @@ namespace Vereesa.Core.Services
 			{
 				if (await TryAnnounceReminderAsync(reminder))
 				{
-					await RemoveReminderAsync(reminder);
+					if (reminder.IsPeriodic)
+					{
+						reminder.RemindTime = Instant.FromUnixTimeSeconds(reminder.RemindTime)
+							.Plus(Duration.FromSeconds(reminder.Interval))
+							.ToUnixTimeSeconds();
+
+						await UpdateReminderAsync(reminder);
+					}
+					else
+					{
+						await RemoveReminderAsync(reminder);
+					}
 				}
 			}
 		}
@@ -170,6 +174,11 @@ namespace Vereesa.Core.Services
 		private async Task RemoveReminderAsync(Reminder reminder)
 		{
 			await _reminderRepository.DeleteAsync(reminder);
+		}
+
+		private async Task UpdateReminderAsync(Reminder reminder)
+		{
+			await _reminderRepository.AddOrEditAsync(reminder);
 		}
 
 		private async Task<bool> TryAnnounceReminderAsync(Reminder reminder)
@@ -184,7 +193,15 @@ namespace Vereesa.Core.Services
 					return true;
 				}
 
-				await channel.SendMessageAsync($"<@{reminder.UserId}> Remember! {reminder.Message}");
+				if (!reminder.IsPeriodic)
+				{
+					await channel.SendMessageAsync($"<@{reminder.UserId}> Remember! {reminder.Message}");
+				}
+				else
+				{
+					// Periodic reminders have the target in the message.
+					await channel.SendMessageAsync($"{reminder.Message}");
+				}
 			}
 			catch
 			{
@@ -197,6 +214,115 @@ namespace Vereesa.Core.Services
 		private async Task<List<Reminder>> GetOverdueRemindersAsync()
 		{
 			return (await _reminderRepository.GetAllAsync()).Where(r => r.RemindTime < DateTimeOffset.UtcNow.ToUnixTimeSeconds()).ToList();
+		}
+
+		[AsyncHandler]
+		[OnCommand("!reminder delete")]
+		public async Task DeleteReminder(IMessage message)
+		{
+			var reminders = (await _reminderRepository.GetAllAsync())
+				.Where(r => r.UserId == message.Author.Id)
+				.ToList();
+
+			if (!reminders.Any())
+			{
+				await message.Channel.SendMessageAsync("You have no active reminders! ✨");
+				return;
+			}
+
+			var reminderSelction = $"Which reminder would you like me to delete?\n" +
+				string.Join("\n", reminders.Select((item, index) =>
+					$"[{index + 1}] {item.Message}"
+				)) + "\n" +
+				"Select by responding with the number corresponding to the item you want to remove. Like this `1`.";
+
+			var response = await Prompt(message.Author, reminderSelction, message.Channel, 30000);
+
+			if (
+				!int.TryParse(response.Content, out var selectedItemNumber) ||
+				reminders.Count < selectedItemNumber ||
+				selectedItemNumber < 1
+			)
+			{
+				await message.Channel.SendMessageAsync("I don't understand which reminder you mean...");
+				return;
+			}
+
+			var itemToDelete = reminders[selectedItemNumber - 1];
+			await RemoveReminderAsync(itemToDelete);
+
+			await message.Channel.SendMessageAsync("OK, I deleted that reminder! ✨");
+		}
+
+		[AsyncHandler]
+		[OnCommand("!reminder create periodic")]
+		[Authorize("Officer")]
+		public async Task CreatePeriodicReminder(IMessage message)
+		{
+			var abort = new Func<string, Task>(async (reason) =>
+			{
+				await message.Channel.SendMessageAsync($"❌ {reason} Please try again.");
+			});
+
+			var prompt = new Func<string, int, Task<IMessage>>(async (promptMessage, timeout) =>
+			{
+				return await Prompt(message.Author, $"🎗 {promptMessage}", message.Channel, timeout);
+			});
+
+			var intervalLength = await prompt("How long between each reminder?", 15000);
+			if (!TryParseDuration(intervalLength.Content, out var duration))
+			{
+				await abort("I wasn't able to figure out the interval. Please try again.");
+				return;
+			}
+
+			var startTime = await prompt(
+				"When should the first reminder be? I run on Karazhan realm time. Format it like this `2021-07-09 22:19:01`.",
+				60000
+			);
+			ZonedDateTime dateTime;
+			try
+			{
+				dateTime = startTime.Content.ParseToZonedDateTime("yyyy-MM-dd HH:mm:ss", "Europe/Paris");
+			}
+			catch
+			{
+				await abort("I wasn't able to figure out the start time.");
+				return;
+			}
+
+			var reminderTarget = await prompt("Who should I remind?", 15000);
+			if (reminderTarget == null)
+			{
+				await abort("I wasn't able to figure out who to remind.");
+				return;
+			}
+
+			var reminderText = await prompt("What should the reminder say?", 60000);
+			if (reminderText == null)
+			{
+				await abort("I wasn't able to figure out what the reminder should say.");
+				return;
+			}
+
+			var channel = await prompt("What channel should the reminder be sent to?", 30000);
+			if (channel == null || !ulong.TryParse(channel.Content.Replace("<#", "").Replace(">", "").Trim(), out var channelId))
+			{
+				await abort("I wasn't able to figure out what channel to send the reminder to. " + channel?.Content);
+				return;
+			}
+
+			var reminder = new Reminder();
+			reminder.IsPeriodic = true;
+			reminder.Interval = duration.TotalSeconds;
+			reminder.RemindTime = dateTime.ToInstant().ToUnixTimeSeconds();
+			reminder.UserId = message.Author.Id;
+			reminder.ChannelId = channelId;
+			reminder.Message = $"{reminderTarget.Content}: {reminderText.Content}";
+			await AddReminderAsync(reminder);
+
+			await message.Channel.SendMessageAsync(
+				$"OK, I'll remind {reminderTarget.Content} that {reminderText.Content} every {duration} starting on {dateTime}.");
 		}
 	}
 }
--- Vereesa.Data/Models/Reminders/Reminder.cs
+++ Vereesa.Data/Models/Reminders/Reminder.cs
@@ -3,17 +3,19 @@ using Vereesa.Data.Interfaces;
 
 namespace Vereesa.Data.Models.Reminders
 {
-    public class Reminder : IEntity
-    {
-        public Reminder() 
-        {
-            Id = Guid.NewGuid().ToString();
-        }
+	public class Reminder : IEntity
+	{
+		public Reminder()
+		{
+			Id = Guid.NewGuid().ToString();
+		}
 
-        public string Id { get; set; }
-        public ulong UserId { get; set; }
-        public long RemindTime { get; set; }
-        public string Message { get; set; }
-        public ulong ChannelId { get; set; }
-    }
+		public string Id { get; set; }
+		public ulong UserId { get; set; }
+		public long RemindTime { get; set; }
+		public string Message { get; set; }
+		public ulong ChannelId { get; set; }
+		public bool IsPeriodic { get; set; }
+		public double Interval { get; set; }
+	}
 }
