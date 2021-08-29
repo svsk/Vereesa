b476f3c20afd7d16db161621ff8586e5d8caa68c
Sverre Skuland <sverre.skuland@gmail.com>
2021-08-29 13:47:30 +0200

    Fix unit test buld error.

--- Vereesa.Core.Tests/ServicesTests/RaidSplitServiceTests.cs
+++ Vereesa.Core.Tests/ServicesTests/RaidSplitServiceTests.cs
@@ -26,7 +26,7 @@ namespace Vereesa.Core.Tests.ServicesTests
 
 			var service = new RaidSplitService(discordMock.Object, warcraftLogsApi.Object);
 
-			await service.SplitRaidEvenlyAsync(messageMock.Object, "2");
+			await service.SplitRaidEvenlyAsync(messageMock.Object, "2", "");
 
 			Assert.Equal("Fenriz, Eyrie, Azibo, Zyx, Xevlicious", returnedMessage);
 		}
