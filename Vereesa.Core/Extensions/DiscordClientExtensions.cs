dc696205397446fbfebf3a1c8a5a12a007480ec5
Sverre Skuland <sverre.skuland@gmail.com>
2023-08-16 16:48:44 +0200

    Apply code formatting.

--- Vereesa.Core/Extensions/DiscordClientExtensions.cs
+++ Vereesa.Core/Extensions/DiscordClientExtensions.cs
@@ -8,60 +8,85 @@ using Discord.WebSocket;
 
 namespace Vereesa.Core.Extensions
 {
-	public static class DiscordClientExtensions
-	{
-		public static async Task<IMessageChannel> GetGuildChannelByNameAsync(this DiscordSocketClient client, string guildName, string channelName)
-		{
-			IReadOnlyCollection<IGuild> guilds = client.Guilds;
-			List<IGuild> matchingGuilds = guilds.Where(g => g.Name == guildName).ToList();
+    public static class DiscordClientExtensions
+    {
+        public static async Task<IMessageChannel> GetGuildChannelByNameAsync(
+            this DiscordSocketClient client,
+            string guildName,
+            string channelName
+        )
+        {
+            IReadOnlyCollection<IGuild> guilds = client.Guilds;
+            List<IGuild> matchingGuilds = guilds.Where(g => g.Name == guildName).ToList();
 
-			if (matchingGuilds.Count > 1)
-			{
-				throw new InvalidOperationException("More than one guild with the same name found.");
-			}
+            if (matchingGuilds.Count > 1)
+            {
+                throw new InvalidOperationException("More than one guild with the same name found.");
+            }
 
-			if (matchingGuilds.Count == 0)
-			{
-				throw new InvalidOperationException("No guilds with that name found.");
-			}
+            if (matchingGuilds.Count == 0)
+            {
+                throw new InvalidOperationException("No guilds with that name found.");
+            }
 
-			IGuild guild = matchingGuilds.First();
-			channelName = channelName.Replace("#", string.Empty);
-			IReadOnlyCollection<IChannel> channels = await guild.GetChannelsAsync();
-			List<IChannel> matchingChannels = channels.Where(c => c.Name == channelName).ToList();
+            IGuild guild = matchingGuilds.First();
+            channelName = channelName.Replace("#", string.Empty);
+            IReadOnlyCollection<IChannel> channels = await guild.GetChannelsAsync();
+            List<IChannel> matchingChannels = channels.Where(c => c.Name == channelName).ToList();
 
-			if (matchingChannels.Count > 1)
-			{
-				throw new InvalidOperationException("More than one channel with the same name found.");
-			}
+            if (matchingChannels.Count > 1)
+            {
+                throw new InvalidOperationException("More than one channel with the same name found.");
+            }
 
-			if (matchingChannels.Count == 0)
-			{
-				throw new InvalidOperationException("No channels with that name found.");
-			}
+            if (matchingChannels.Count == 0)
+            {
+                throw new InvalidOperationException("No channels with that name found.");
+            }
 
-			IMessageChannel messageChannel = matchingChannels.First() as IMessageChannel;
+            IMessageChannel messageChannel = matchingChannels.First() as IMessageChannel;
 
-			if (messageChannel == null)
-			{
-				throw new InvalidCastException("Could not cast channel to IMessageChannel.");
-			}
+            if (messageChannel == null)
+            {
+                throw new InvalidCastException("Could not cast channel to IMessageChannel.");
+            }
 
-			return messageChannel;
-		}
+            return messageChannel;
+        }
 
-		public static SocketRole GetRole(this DiscordSocketClient discord, ulong roleId)
-		{
-			return discord.Guilds.SelectMany(g => g.Roles).FirstOrDefault(r => r.Id == roleId);
-		}
+        public static SocketRole GetRole(this DiscordSocketClient discord, ulong roleId)
+        {
+            return discord.Guilds.SelectMany(g => g.Roles).FirstOrDefault(r => r.Id == roleId);
+        }
 
-		public static List<SocketRole> GetRolesByName(this DiscordSocketClient discord, string roleName, bool ignoreCase = true)
-		{
-			return discord.Guilds.SelectMany(g => g.Roles).Where(r =>
-				r.Name.Equals(roleName, ignoreCase ?
-					StringComparison.InvariantCultureIgnoreCase :
-					StringComparison.InvariantCulture)
-				).ToList();
-		}
-	}
+        public static List<SocketRole> GetRolesByName(
+            this DiscordSocketClient discord,
+            string roleName,
+            bool ignoreCase = true
+        )
+        {
+            return discord.Guilds
+                .SelectMany(g => g.Roles)
+                .Where(
+                    r =>
+                        r.Name.Equals(
+                            roleName,
+                            ignoreCase ? StringComparison.InvariantCultureIgnoreCase : StringComparison.InvariantCulture
+                        )
+                )
+                .ToList();
+        }
+
+        public static string GetPreferredDisplayName(this IUser author)
+        {
+            if (author is IGuildUser guildUser)
+            {
+                return guildUser.DisplayName ?? guildUser.Username;
+            }
+            else
+            {
+                return author.Username;
+            }
+        }
+    }
 }
