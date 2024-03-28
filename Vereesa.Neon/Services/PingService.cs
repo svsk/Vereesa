485846cd25939cb029387037dac8d6debb3b13d6
Sverre Skuland <sverre.skuland@gmail.com>
2024-03-28 15:39:28 +0100

    Add ping slash command.

--- Vereesa.Neon/Services/PingService.cs
+++ Vereesa.Neon/Services/PingService.cs
@@ -3,11 +3,19 @@ using Discord;
 using RestSharp;
 using Vereesa.Core.Infrastructure;
 using Vereesa.Core;
+using Discord.Interactions;
 
 namespace Vereesa.Neon.Services
 {
     public class PingService : IBotService
     {
+        private readonly IMessagingClient _messagingClient;
+
+        public PingService(IMessagingClient messagingClient)
+        {
+            _messagingClient = messagingClient;
+        }
+
         [OnCommand("!ping")]
         [Description("Ping Vereesa to check if she is still alive and accepting commands.")]
         public async Task HandleMessageAsync(IMessage message)
@@ -15,6 +23,29 @@ namespace Vereesa.Neon.Services
             var responseMessage = await message.Channel.SendMessageAsync($"Pong!");
             var responseTimestamp = responseMessage.Timestamp.ToUnixTimeMilliseconds();
             var messageSentTimestamp = message.Timestamp.ToUnixTimeMilliseconds();
+            await responseMessage.ModifyAsync(
+                (msg) =>
+                {
+                    msg.Content = $"Pong! (Responded after {responseTimestamp - messageSentTimestamp} ms)";
+                }
+            );
+        }
+
+        [SlashCommand("ping", "Ping Vereesa to check if she is still alive and accepting commands.")]
+        public async Task HandleInteractionAsync(IDiscordInteraction interaction)
+        {
+            var start = DateTimeOffset.Now;
+            await interaction.DeferAsync();
+
+            if (interaction.ChannelId == null)
+            {
+                return;
+            }
+
+            var responseMessage = await interaction.FollowupAsync("Pong!");
+            var responseTimestamp = responseMessage.Timestamp.ToUnixTimeMilliseconds();
+            var messageSentTimestamp = start.ToUnixTimeMilliseconds();
+
             await responseMessage.ModifyAsync(
                 (msg) =>
                 {
